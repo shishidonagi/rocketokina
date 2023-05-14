241a8c04b83d5dd25036d85571bdf9d9a260b3d2
shishidonagi <98552331+shishidonagi@users.noreply.github.com>
2023-05-14 12:28:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -108,10 +108,15 @@ window.onload = function () {
             mainScene.onenterframe = function () {
 
                 if(state == 1 || state == 2){
-                    if(bgm == 0){
+		    if(bgm == 0){
                         game.assets[BGMUrl].play();
-                        bgm = 1;
+		    };
+                    if(bgm < 3050){
+			bgm += 1;
                     };
+		    if(bgm == 3050){
+			bgm = 0;
+		    }
                 }
 
                 if(state == 2){
